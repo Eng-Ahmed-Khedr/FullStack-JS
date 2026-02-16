b6769c3825d30e1487d2d44dd0e9ad27b3490c67
Ahmed Ayman Khedr <94747792+Developer-Ahmed-Ayman-khedr@users.noreply.github.com>
2026-02-16 19:16:38 +0200

    Update global rules and upload session 17 JavaScript.

---
+++ frontend/session-17-JavaScript/js/script.js
@@ -0,0 +1,4 @@
+let userName = prompt("Enter your name:")
+console.log("Your name is " + userName + "Its type " + typeof userName)
+let userAge = +prompt("Enter your age:")
+console.log("Your age is " + userAge + " . " + (userAge >= 18) + " is its status and its type is " + typeof userAge)
